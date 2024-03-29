55f701665c0ac7ac441f2f0df5b77b66ad681e77
aaridhon19 <adamrizqi.arr@gmail.com>
2024-03-29 17:25:27 +0700

    fix apollo.js

--- app/config/apollo.js
+++ app/config/apollo.js
@@ -1,7 +1,24 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import * as SecureStore from "expo-secure-store";
+
+const httpLink = createHttpLink({
+  uri: "https://dd7b-139-228-111-126.ngrok-free.app",
+});
+
+const authLink = setContext(async (_, { headers }) => {
+  const token = await SecureStore.getItemAsync("accessToken");
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: "https://dd7b-139-228-111-126.ngrok-free.app",
+  uri: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
